51ce6f818a98e5825991c33522a2e4d54333895d
FaziletKosure <faziletkosure1@gmail.com>
2020-11-03 17:54:58 +0100

    add src folder

---
+++ src/Router.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Router = () => {
+  return (
+    <View>
+      <Text />
+    </View>
+  );
+};
+
+export default Router;
+
+const styles = StyleSheet.create({});
