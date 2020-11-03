55ae1ab48c87086576ecc2a2911ec67299c405db
FaziletKosure <faziletkosure1@gmail.com>
2020-11-03 17:59:32 +0100

    add router file

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
 const Router = () => {
   return (
-    <View>
-      <Text />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options={{headerShown: false}} />
+        <Stack.Screen />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
